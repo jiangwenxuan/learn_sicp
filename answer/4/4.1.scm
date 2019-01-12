014b1092effa881136bb5d97f5cf88ec06e737e2
jwx <904653684@qq.com>
2019-01-12 20:59:44 +0800

    eval and apply

--- answer/4/4.1.scm
+++ answer/4/4.1.scm
@@ -1,1 +1,15 @@
 #lang sicp
+
+(define (list-of-values exps env)
+  (if (no-operands? exps)
+      '()
+      (let ((left (eval (first-operand exp) env)))
+        (let ((right (list-of-values (rest-operands exps) env)))
+          (cons left right)))))
+
+(define (new-list-of-values exps env)
+  (if (no-operands? exps)
+      '()
+      (let ((right (list-of-values (rest-operands exps) env)))
+        (let ((left (eval (first-operand exp) env)))
+          (cons left right)))))
